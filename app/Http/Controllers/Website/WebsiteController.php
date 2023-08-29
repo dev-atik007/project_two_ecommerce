2312c4c3f6ad27c6bea713e71ec8a62a1530bb52
dev-atik007 <atikur888setu@gmail.com>
2023-08-29 20:37:41 +0600

    Show Product Data to UserPage

--- app/Http/Controllers/Website/WebsiteController.php
+++ app/Http/Controllers/Website/WebsiteController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Website;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class WebsiteController extends Controller
 {
     public function home()
     {
-        return view('website.home');
+        $product=Product::paginate(6);
+        return view('website.home', compact('product'));
     }
 }
--- resources/views/admin/product/editProduct.blade.php
+++ resources/views/admin/product/editProduct.blade.php
@@ -71,7 +71,7 @@
                     </div>
                     <div class="div_design">
                         <label>Discount Price :</label>
-                        <input class="text_color" type="number" name="dis_price" value="{{ $product->discount_price }}" required>
+                        <input class="text_color" type="number" name="dis_price" value="{{ $product->discount_price }}">
                     </div>
                     <div class="div_design">
                         <label>Product Qty :</label>
